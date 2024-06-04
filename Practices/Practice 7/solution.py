4d4be3ca56690951cdf97f1512576035cee36b8b
Bardin08 <vbardin810@gmail.com>
2024-06-04 14:39:13 +0300

    Update practice 7 solution

--- Practices/Practice 7/solution.py
+++ Practices/Practice 7/solution.py
@@ -1,29 +1,31 @@
 import random
 
-words = ["apple", "rapid", "panda", "hello", "world", "grape", "melon"]
-secret_word = random.choice(words)
+MAX_ATTEMPTS = 6
+WORDS = ["apple", "rapid", "panda", "hello", "world", "grape", "melon"]
+secret_word = random.choice(WORDS)
 
 print("Secret word: ", secret_word)
 
-attempts = 0
-while attempts < 5:
-    attempts += 1
-    guess = input("Enter your guess: ")
-    while not (len(guess) == 5 and guess in words):
+attempt = 0
+while attempt < MAX_ATTEMPTS:
+    attempt += 1
+    guess = input(f"Enter your guess ({attempt}💔\\{MAX_ATTEMPTS}): ")
+    while not (len(guess) == 5 and guess in WORDS):
         print("Invalid guess word. Try again.")
         guess = input("Enter your guess: ")
 
     if guess == secret_word:
-        print("You guessed correctly!")
-        break
+        print("You guessed correctly!👏")
 
     zipped_words = zip(guess, secret_word)
     for g, h in zipped_words:
         if g == h:
-            print("Letter matched: ", g)
+            print(str(g).upper(), end="")
         elif g in secret_word:
-            print("Letter not matched(position): ", g)
+            print(g, end="")
         else:
-            print("Letter not matched: ", g)
+            print("*", end="")
+    print()
 else:
-    print("You have used all your attempts. The secret word was.")
+    print("You lost!😔")
+
